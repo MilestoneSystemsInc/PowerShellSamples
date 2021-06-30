cf6494834831dad23920651235cd543eaa9d4450
Josh Hendricks <jhendricks123@gmail.com>
2021-06-29 21:35:27 -0700

    Refactor Get-UsersInRoles

--- Samples/Get-UsersInRoles.ps1
+++ Samples/Get-UsersInRoles.ps1
@@ -6,82 +6,45 @@ function Get-UsersInRoles {
         Returns all users/groups, the role they are in, and the type of user (WindowsUser, WindowsGroup, and BasicUser).  WindowsUser
         and WindowsGroup could be either Windows or Active Directory.  If it is a group, it will only display the group.  It will
         not display the users in the group.
-    
-    .PARAMETER $RoleName
-        Allows you to specify the name of a Role.  If left out, it will return info for all roles.
-
     .EXAMPLE
-        Get-UsersInRoles
-        
+        PS C:\> Get-UsersInRoles
         Returns all users/groups from all roles
-        
     .EXAMPLE
-        Get-UsersInRoles -RoleName "Administrators"
-
+        PS C:\> Get-UsersInRoles -RoleName "Administrators"
         Returns all of the users/groups that are in the Administrators role
-
     .EXAMPLE
-        Get-UsersInRoles | Out-GridView
-
+        PS C:\> Get-UsersInRoles | Out-GridView
         Outputs the results to a GridView pop-up.
-    
     .EXAMPLE
-        Get-UsersInRoles | Export-Csv -Path C:\CsvExports\Users.csv -NoTypeInformation
-
+        PS C:\> Get-UsersInRoles | Export-Csv -Path C:\CsvExports\Users.csv -NoTypeInformation
         Exports the information to a CSV file called Users.csv located at C:\CsvExports.  The CsvExports folder must exist before running the command.
     #>
-    
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $false)]
-        [string]$RoleName
+        # Specifies the name of the role. Supports wildcard characters. Default is * and returns all roles.
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $RoleName = '*'
     )
 
     process {
-        $usersInRoles = New-Object System.Collections.Generic.List[PSCustomObject]
-        if ([string]::IsNullOrWhiteSpace($RoleName))
+        [bool]$resultFound
+        foreach ($role in Get-Role -Name $RoleName)
         {
-            $roles = Get-Role
-        } else
-        {
-            $roles = Get-Role -Name $RoleName
-            if ([string]::IsNullOrWhiteSpace($roles))
+            $resultFound = $true
+            foreach ($user in $role.UserFolder.Users)
             {
-                Write-Host "Role name does not exist.  Please try again." -ForegroundColor Yellow
-                Break
-            }
-        }
-        
-        foreach ($role in $roles)
-        {
-            $users = $role.UserFolder.Users
-
-            foreach ($user in $users)
-            {
-                $row = [PSCustomObject]@{
-                    'Role' = $role.Name
-                    'User' = $user.AccountName
-                    'UserType' = $user.IdentityType
+                [pscustomobject]@{
+                    Role = $role.Name
+                    User = $user.AccountName
+                    Domain = $user.Domain
+                    IdentityType = $user.IdentityType
                 }
-                $usersInRoles.Add($row)
             }
         }
-        $usersInRoles
+        if (-not $resultFound -and -not [system.management.automation.wildcardpattern]::ContainsWildcardCharacters($RoleName)) {
+            Write-Error "Cannot find role '$RoleName' because it does not exist."
+        }
     }
 }
-
-<#
-.SYNOPSIS
-    Short description
-.DESCRIPTION
-    Long description
-.EXAMPLE
-    PS C:\> <example usage>
-    Explanation of what the example does
-.INPUTS
-    Inputs (if any)
-.OUTPUTS
-    Output (if any)
-.NOTES
-    General notes
-#>
